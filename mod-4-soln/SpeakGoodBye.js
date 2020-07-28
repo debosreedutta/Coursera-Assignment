16d9253b02a8d23e24f67a16255be99c9c33d841
Debosree Dutta <debosreedutta096@gmail.com>
2020-07-28 20:43:24 +0530

    Update SpeakGoodBye.js

--- mod-4-soln/SpeakGoodBye.js
+++ mod-4-soln/SpeakGoodBye.js
@@ -1,1 +1,8 @@
-h
+(function(window) {
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
+})(window);
